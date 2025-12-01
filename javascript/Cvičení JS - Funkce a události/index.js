98aa3057929e237cecc8a32e441042260865de31
neutron1331 - school <[valid-email]>
2025-12-01 11:07:25 +0100

    another js tasks

---
+++ javascript/Cvičení JS - Funkce a události/index.js
@@ -0,0 +1,68 @@
+/* 31) Po kliknutí na tlačítko zobraz alert „Ahoj světe!“.
+      - Vytvoř funkci, která zobrazí alert.
+      - Použij událost onclick nebo addEventListener.
+*/
+
+document.getElementById("Alert").addEventListener("click", alert_func_u31);
+function alert_func_u31()
+{
+    alert("Ahoj světe!");
+}
+
+/* 32) Po kliknutí na tlačítko změň text vybraného HTML elementu.
+      - Napiš funkci, která změní .textContent nebo .innerText.
+      - Vyzkoušej použití arrow funkce.
+*/
+
+function Text_change_u32()
+{
+    add = (a,b) => a+b;
+    change = () => "změněný text";
+    document.getElementById("Text_u32").innerText = change();
+}
+
+/* 33) Vypiš do <div> aktuální čas, který se bude každou sekundu aktualizovat.
+      - Použij setInterval a vlastní funkci pro získání času.
+      - Funkce by měla vracet formátovaný čas (HH:MM:SS).
+*/
+
+function Time_func_u33()
+{
+    let time = new Date();
+    document.getElementById("Time_u33").innerHTML = time;
+}
+
+/* 34) Po kliknutí na tlačítko změň barvu pozadí celé stránky.
+      - Vytvoř funkci s parametrem (barva).
+      - Vyzkoušej nastavit hodnotu přes document.body.style.
+*/
+
+/* 35) Po najetí myší na obrázek ho zvětši.
+      - Použij eventy mouseover a mouseout.
+      - Změnu proveď pomocí třídy v CSS nebo style.width v JS.
+*/
+
+/* 36) Po odeslání formuláře vypiš hodnoty inputů do konzole.
+      - Zakázat přirozené odeslání formuláře pomocí event.preventDefault().
+      - Použij funkci, která načte hodnoty inputů.
+*/
+
+/* 37) Vytvoř jednoduché „počítadlo kliknutí“.
+      - Po každém kliknutí zvýší číslo o 1 a zobraz ho v HTML.
+      - Proměnná se musí nacházet ve správném scope.
+*/
+
+/* 38) Po kliknutí na tlačítko skryj nebo zobraz text.
+      - Můžeš použít classList.toggle().
+      - Zvaž použití anonymní nebo arrow funkce.
+*/
+
+/* 39) Validuj formulář – zkontroluj, jestli je vyplněné jméno a e-mail.
+      - Pokud není validní, zobraz chybovou hlášku.
+      - Použij jednoduchou podmínku a funkci validateForm().
+*/
+
+/* 40) Pomocí slideru (input type="range") měň velikost písma textu.
+      - Po změně hodnoty aktualizuj style.fontSize.
+      - Hodnota ze slideru bude vstupním parametrem funkce.
+*/
