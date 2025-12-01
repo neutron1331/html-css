e79a7f9aacefe9517047a059c59f2853c6ef988b
neutron1331 - school <[valid-email]>
2025-12-01 15:00:50 +0100

    zápis

---
+++ javascript/zápis/index.js
@@ -0,0 +1,53 @@
+/*
+
+pole: 
+
+    let pole = [1,2,3]
+
+    push - přidá dalí pole do pole
+        pole.push(4) // pole je nyní [1,2,3,4]
+
+    pop - odstraní poslední prvek pole
+        pole.pop() // pole je nyní [1,2,3]
+
+    shift - posune pole doprava
+        pole.shift() // pole je nyní [2,3]
+
+    map - vytvoří nové pole s upravenými prvky
+        pole.map(x => x * 2) // vrátí [2,4,6]
+
+    filter - vytvoří nové pole s prvky které splňují podmínku
+        pole.filter(x => x > 1) // vrátí [2,3]
+
+    find - najde první prvek který splňuje podmínku
+        pole.find(x => x > 1) // vrátí 2
+
+    sort - seřadí pole
+        pole.sort((a,b) => b - a) // pole je nyní [3,2,1]
+
+    count - vrátí počet prvků v poli
+        pole.length // vrátí 3
+
+obj:
+    
+    let obj = {a: 1, b: 2, c: 3} // klíč: hodnota
+    let obj = {id: 1, name: "Jan", age: 30}
+
+    k hodnotě se odkazujeme přes klíč
+
+kolekce:
+
+    let kolekce = []
+
+Data:d = new Date()
+
+
+Funkce:
+
+    delkarace
+    parametry a returns
+    arrow funkce
+    scope
+    default parametry
+    callback funkce
+*/
