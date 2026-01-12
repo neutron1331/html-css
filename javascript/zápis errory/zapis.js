75eded406a5b458d1ad1f5d3d1728d7b0d407813
neutron1331 - school <[valid-email]>
2026-01-12 15:17:24 +0100

    errors

---
+++ javascript/zápis errory/zapis.js
@@ -0,0 +1,11 @@
+/*
+
+error handling
+
+try {}          // code that may throw an error
+
+catch (e) {}    // code to handle the error
+
+finally {}      // code that runs regardless of an error occurring
+
+*/
