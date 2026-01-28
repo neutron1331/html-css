ddfabff912b5a89df4743f3aae28938314da4e14
neutron1331 - school <[valid-email]>
2026-01-28 12:27:12 +0100

    přesunutí zpátky

---
+++ PHP/úlohy/php.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    #proměnné
+
+    $prom = "ahoj světe";
+    print $prom.'<br>';
+    echo $prom.'hoj';
+
+    # dat typ
+
+    print '<br><h1>Datové tipy<br>Celé čísla: ';
+    $x = 5;
+    var_dump(value: $x);
+
+    print '<br>řetězce: ';
+    $x = 'asdf';
+    var_dump(value: $x);
+
+    print '<br>desetinní čísla: ';
+    $x = 1.1;
+    var_dump(value: $x);
+
+    print '<br>pravdivostní hodnota: ';
+    $x = true;
+    var_dump(value: $x);
+
+    print '<br>řetězce: ';
+    $x = 'asdf';
+    var_dump(value: $x);
+
+    print '<br>řetězce: ';
+    $x = 'asdf';
+    var_dump(value: $x);
+?>    
+
+
+</body>
+</html>
