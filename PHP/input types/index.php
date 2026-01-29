470050edb6641242c03449a0375ea88364caa711
neutron1331 - school <[valid-email]>
2026-01-29 09:24:45 +0100

    php input types

---
+++ PHP/input types/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="output.php" method="post">
+    1. Text: <input type="text" name="text"><br><br>
+    2. Password: <input type="password" name="password"><br><br>
+    3. Email: <input type="email" name="email"><br><br>
+    4. Number: <input type="number" name="number"><br><br>
+    5. Tel: <input type="tel" name="tel"><br><br>
+    6. URL: <input type="url" name="url"><br><br>
+    7. Search: <input type="search" name="search"><br><br>
+    8. Hidden: <input type="hidden" name="hidden" value="hidden_value"><br><br>
+    9. Date: <input type="date" name="date"><br><br>
+    10. Datetime-local: <input type="datetime-local" name="datetime_local"><br><br>
+    11. Month: <input type="month" name="month"><br><br>
+    12. Week: <input type="week" name="week"><br><br>
+    13. Time: <input type="time" name="time"><br><br>
+    14. Color: <input type="color" name="color"><br><br>
+    15. Range: <input type="range" name="range" min="0" max="100"><br><br>
+    16. Checkbox: <input type="checkbox" name="checkbox" value="checked"><br><br>
+    17. Radio: 
+        <input type="radio" name="radio" value="option1"> Option 1
+        <input type="radio" name="radio" value="option2"> Option 2<br><br>
+    18. File: <input type="file" name="file"><br><br>
+    19. Submit: <input type="submit" name="submit" value="Submit"><br><br>
+    20. Reset: <input type="reset" value="Reset"><br><br>
+    21. Button: <input type="button" value="Click Me" onclick="alert('Button clicked!')"><br><br>
+    22. Image: <input type="image" src="https://via.placeholder.com/50" alt="Submit" name="image"><br><br>
+    <input type="submit">    
+</form>
+</body>
+</html>
---
+++ PHP/input types/output.php
@@ -0,0 +1,37 @@
+<?php
+
+############## text ##############
+
+$input = $_POST["text"] ?? false;
+
+############## passwd ##############
+
+$input = $_POST["password"] ?? false;
+
+############## email ##############
+
+$input = $_POST["email"];
+
+############## number ##############
+
+$input = $_POST["number"];
+
+############## tel ##############
+
+$input = $_POST["tel"];
+
+############## url ##############
+
+$input = $_POST["url"];
+
+############## search ##############
+
+$input = $_POST["text"];
+
+############## hidden ##############
+
+$input = $_POST["text"];
+
+
+
+?>
