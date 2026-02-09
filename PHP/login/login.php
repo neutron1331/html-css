043e7a08d83a58ba28c8c1c81b944025cd63e5f0
neutron1331 - school <[valid-email]>
2026-02-09 15:01:11 +0100

    more PHP login

--- PHP/login/login.php
+++ PHP/login/login.php
@@ -1,5 +1,8 @@
 <?php
 
+if (!isset($_POST["user_name"])) {$_POST["user_name"] = "";}
+if (!isset($_POST["user_password"])) {$_POST["user_password"] = "";}
+
 echo "Jméno: ".$_POST["user_name"]."<br>";
 echo "Heslo: ".$_POST["user_password"]."<br>";
 
@@ -11,26 +14,26 @@ if (($_POST["user_name"]) == "admin" && $_POST["user_password"] == "admin") //is
     echo "uživatel je autorizovaný";
 }
 
+//session a cookies fungujou podobně, cookies spravuje javascript
+
 if (isset($_SESSION["user_name"])) 
 {
     $name = $_SESSION["user_name"];
-    $paswd = $_SESSION["user_password"];
-    echo 
-    '
-    <form action="" method="get">
-        <input type="submit" name="logout" value="Odhlásit se">
-    </form>
-    ';
-    echo isset($_GET["logout"]) ? "odhlásit" : "neodhlásit";
-    if (isset($_GET["logout"]))
-    {
-        session_unset();
-        session_destroy();
-    }
+    $paswd = $_SESSION["user_password"];    
 }
 else
 {
     echo "uživatel není autorizovaný";
 }
-
+echo 
+    '
+    <form action="" method="get">
+        <input type="submit" name="logout" value="Odhlásit se">
+    </form>
+    ';
+if (isset($_GET["logout"]))
+{
+    session_unset();
+    session_destroy();
+}
 ?>
